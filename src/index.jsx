3e6993c62846f443bccdb91810261285bd3e05e6
rumpeltin <76797616+rumpeltin@users.noreply.github.com>
2023-02-17 15:26:10 +0000

    added basic structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MainView } from './components/main-view/main-view';
+import { Greeting } from './components/greeting/greeting';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
 // Main component (will eventually use all the others)
-class MyFlixApplication extends React.Component {
+class DeppFlixApplication extends React.Component {
   render() {
     return (
-      <div className="my-flix">
-        <div>Good morning</div>
-      </div>
+      <>
+        <Greeting/>
+        <MainView/>
+      </>
     );
   }
 }
@@ -19,4 +22,4 @@ class MyFlixApplication extends React.Component {
 const container = document.getElementsByClassName('app-container')[0];
 
 // Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication), container);
+ReactDOM.render(React.createElement(DeppFlixApplication), container);
